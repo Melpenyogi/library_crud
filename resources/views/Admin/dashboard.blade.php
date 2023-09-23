eba4a4ab7684df0133238f18234b4b02f94e4186
melpenyogi <melpenyogi2019@gmail.com>
2023-09-23 07:18:21 +0700

    Aplikasi CRUD Laravel

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+<h1>Dashboard Admin </h1>
+
+<a href="{{ url('admin/news') }}"> News </a> <br>
+<a href="{{ url('admin/reports') }}"> Reports </a> <br>
+<a href="{{ url('admin/settings') }}"> Settings </a> <br>
+<a href="{{ url('admin/user') }}"> User </a> <br>
